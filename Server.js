fcfc8dd73ac725f9dcef37edb7f00f4146c0066d
AnujWebDev <anujyash10gmail.com>
2024-01-28 18:08:52 +0530

    added CORS

--- Server.js
+++ Server.js
@@ -3,11 +3,21 @@ import mongoose from "mongoose";
 import bodyParser from "express";
 import { config } from "dotenv";
 import userRouter from "./Routes/user.js"
+import cors from 'cors';
+
 
 const app = express();
 
 // creating config file
 config({ path: ".env" });
+
+app.use(
+  cors({
+    origin:"https://dashboard-p2f2.vercel.app",
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+  })
+)
 
 app.use(bodyParser.json());
 
